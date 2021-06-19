e228b298192cb83181f09d6cc741636cfe44da19
monologg <adieujw@gmail.com>
2021-06-19 18:01:27 +0000

    chore: add __version__

--- ko_lm_dataformat/__init__.py
+++ ko_lm_dataformat/__init__.py
@@ -14,6 +14,10 @@ import jsonlines
 import ujson as json
 import zstandard
 
+version_txt = os.path.join(os.path.dirname(__file__), "version.txt")
+with open(version_txt) as f:
+    __version__ = f.read().strip()
+
 
 def listdir_or_file(x):
     if isinstance(x, list):
