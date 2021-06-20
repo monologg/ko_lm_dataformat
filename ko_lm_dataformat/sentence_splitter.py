d81ffc1076cc9f77451df502690d265ff02dd9f7
monologg <adieujw@gmail.com>
2021-06-20 13:26:58 +0000

    refactor: move importlib_metadata to utils.py

--- ko_lm_dataformat/sentence_splitter.py
+++ ko_lm_dataformat/sentence_splitter.py
@@ -1,15 +1,9 @@
 import importlib
 import logging
-import sys
 from typing import List
 
 from .sentence_cleaner import clean_sentence
-
-# The package importlib_metadata is in a different place, depending on the python version.
-if sys.version_info < (3, 8):
-    import importlib_metadata
-else:
-    import importlib.metadata as importlib_metadata
+from .utils import importlib_metadata
 
 logger = logging.getLogger(__name__)
 
