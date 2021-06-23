77bcde3edf48dd6fbc0c55f028f25b1d937dbf66
monologg <adieujw@gmail.com>
2021-06-23 05:04:59 +0000

    test: add more test

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -5,13 +5,25 @@ import ko_lm_dataformat as klmd
 from .testing_utils import TMP_DIR_NAME, get_tests_dir, remove_tmp_dir
 
 
-def test_archive_single_sentence():
+def test_kowiki_archive():
     remove_tmp_dir()
     archive = klmd.Archive(TMP_DIR_NAME)
     with open(get_tests_dir(append_path="assets/kowiki_sample.txt"), "r", encoding="utf-8") as f:
         for line in f:
             archive.add_data(line.strip())
     shutil.rmtree(TMP_DIR_NAME)
+
+
+def test_kor_str_is_same():
+    remove_tmp_dir()
+    archive = klmd.Archive(TMP_DIR_NAME)
+    text = open(get_tests_dir(append_path="assets/kowiki_sample.txt"), "r", encoding="utf-8").read()
+    archive.add_data(text)
+    archive.commit()
+
+    reader = klmd.Reader(TMP_DIR_NAME)
+    data = list(reader.stream_data())
+    assert data[0] == text
 
 
 # TODO how to install kss in github workflows
