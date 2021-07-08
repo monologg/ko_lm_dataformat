aa8ec8caa545f5eaa758f7e2b9ba7e05de75decd
monologg <adieujw@gmail.com>
2021-07-08 17:22:36 +0000

    refactor: KssSentenceSplitter -> KssV1SentenceSplitter

--- ko_lm_dataformat/__init__.py
+++ ko_lm_dataformat/__init__.py
@@ -1,6 +1,6 @@
 from .archive import Archive, DatArchive, JSONArchive
 from .reader import Reader
-from .sentence_splitter import KssSentenceSplitter
+from .sentence_splitter import KssV1SentenceSplitter
 from .utils import get_version, tarfile_reader
 
 __version__ = get_version()
