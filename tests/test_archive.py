cd21486c8f25b2eb0e9d6c668412a616d92db157
Jongwon Lee <mybizzer@gmail.com>
2021-07-09 11:12:01 +0900

    refactor: klmd -> kldf

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -1,13 +1,13 @@
 import shutil
 
-import ko_lm_dataformat as klmd
+import ko_lm_dataformat as kldf
 
 from .testing_utils import TMP_DIR_NAME, get_tests_dir, remove_tmp_dir
 
 
 def test_kowiki_archive():
     remove_tmp_dir()
-    archive = klmd.Archive(TMP_DIR_NAME)
+    archive = kldf.Archive(TMP_DIR_NAME)
     with open(get_tests_dir(append_path="assets/kowiki_sample.txt"), "r", encoding="utf-8") as f:
         for line in f:
             archive.add_data(line.strip())
@@ -16,12 +16,12 @@ def test_kowiki_archive():
 
 def test_kor_str_is_same():
     remove_tmp_dir()
-    archive = klmd.Archive(TMP_DIR_NAME)
+    archive = kldf.Archive(TMP_DIR_NAME)
     text = open(get_tests_dir(append_path="assets/kowiki_sample.txt"), "r", encoding="utf-8").read()
     archive.add_data(text)
     archive.commit()
 
-    reader = klmd.Reader(TMP_DIR_NAME)
+    reader = kldf.Reader(TMP_DIR_NAME)
     data = list(reader.stream_data())
     assert data[0] == text
 
@@ -29,7 +29,7 @@ def test_kor_str_is_same():
 # TODO how to install kss in github workflows
 def test_archive_kss_sent_split():
     remove_tmp_dir()
-    archive = klmd.Archive(TMP_DIR_NAME, sentence_splitter=klmd.KssSentenceSplitter())
+    archive = kldf.Archive(TMP_DIR_NAME, sentence_splitter=kldf.KssSentenceSplitter())
     with open(get_tests_dir(append_path="assets/kowiki_sample.txt"), "r", encoding="utf-8") as f:
         for line in f:
             archive.add_data(line.strip(), split_sent=True, clean_sent=False)
